a1e8a660efd2573f884f8f14187f0a03a5c7e05c
Aytixel <aytixel@gmail.com>
2022-09-28 20:26:35 +0200

    Fixed permission to pass headers to predefined status

--- src/runnable/www/test.json.ts
+++ src/runnable/www/test.json.ts
@@ -6,9 +6,10 @@ export default function (
   _app: AppRunner,
   _request: Request,
   _routerData: RouterData,
-  _headers: Record<string, string>,
+  headers: Record<string, string>,
 ) {
   return new Ok200(
     new TextEncoder().encode(`{"time": ${Date.now()}}`),
+    { headers },
   );
 }
